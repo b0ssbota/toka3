43c1e5807806abf5c0b0e62031c1a08739c433c3
b0ssbota <harry2006peter@hotmail.com>
2025-01-31 23:49:27 +0000

    lock in

--- toka/toka/urls.py
+++ toka/toka/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import *
-
+#commit
 urlpatterns = [
     path('', home, name='home'),
     path("admin/", admin.site.urls),
