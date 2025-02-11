8e80726d193a45b155a19593d6d7625062af5f8a
b0ssbota <harry2006peter@hotmail.com>
2025-02-11 19:04:43 +0000

    small chnmage forget to add import

--- toka/toka/views.py
+++ toka/toka/views.py
@@ -1,6 +1,6 @@
 # views.py
 from django.http import HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth import authenticate, login, logout 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User, Group
