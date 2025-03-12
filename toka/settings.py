518e23cf4b06b9c4652a48826fc0e37513bdf357
b0ssbota <harry2006peter@hotmail.com>
2025-03-12 00:24:30 +0000

    test in prod

--- toka/settings.py
+++ toka/settings.py
@@ -150,9 +150,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp-mail.outlook.com'
+EMAIL_HOST = 'smtp.office365.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_HOST_USER = 'harry2006peter@hotmail.com'
 EMAIL_HOST_PASSWORD = 'Meandpeter68#'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
