c7ab090773d0c31fcd820fd875e4f54950142f90
b0ssbota <harry2006peter@hotmail.com>
2025-04-03 00:08:10 +0100

    filter

---
+++ toka/templatetags/math_extras.py
@@ -0,0 +1,22 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def abs(value):
+    # your existing abs filter
+    try:
+        return abs(value)
+    except Exception:
+        return value
+
+@register.filter
+def round_number(value, decimals=0):
+    """
+    Rounds the given value to the specified number of decimals.
+    Usage in template: {{ value|round_number }} or {{ value|round_number:2 }}
+    """
+    try:
+        return round(float(value), int(decimals))
+    except (ValueError, TypeError):
+        return value
